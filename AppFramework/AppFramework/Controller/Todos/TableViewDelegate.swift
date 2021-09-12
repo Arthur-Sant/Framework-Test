efe50a224f131e0e5039bc8381594d6547b7dfa1
Arthur Santiago <71676016+Arthur-Sant@users.noreply.github.com>
2021-09-12 10:17:46 -0300

    deleting useless files

--- AppFramework/AppFramework/Controller/Todos/TableViewDelegate.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension TodosViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-    }
-    
-    
-}
